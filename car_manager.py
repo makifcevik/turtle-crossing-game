f757787388ad327dd2474bd11d69d3d22a6a25e3
makifcevik <makif.cevik02@gmail.com>
2023-10-12 23:36:45 +0300

    Added working cars but no collision yet

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,28 @@
+import random
+from turtle import Turtle
+
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        random_index = random.randint(0, 5)
+        self.color(COLORS[random_index])
+        self.speed("fastest")
+        self.shape("square")
+        self.shapesize(stretch_wid=2, stretch_len=1)
+        self.setheading(90)
+
+        random_y = random.randint(-230, 230)
+        self.goto(300, random_y)
+
+        self.speed_level = 1
+
+    def move(self):
+        self.setx(self.xcor() - STARTING_MOVE_DISTANCE - MOVE_INCREMENT * (self.speed_level - 1))
--- main.py
+++ main.py
@@ -12,9 +12,24 @@ screen.listen()
 player = Player()
 screen.onkeypress(key="Up", fun=player.move)
 
+cars = []
+current_time = time.time()
+
+
+def spawn_car():
+    global current_time
+    if time.time() - 0.3 > current_time:
+        current_time = time.time()
+        new_car = CarManager()
+        cars.append(new_car)
+    for car in cars:
+        car.move()
+
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.040)
+    spawn_car()
     screen.update()
 
 screen.exitonclick()
