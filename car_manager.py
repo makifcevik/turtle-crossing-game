1fab717736a6406ec7950849b1fe06365dae1b3b
makifcevik <makif.cevik02@gmail.com>
2023-10-12 23:51:40 +0300

    Added working cars and win logic

--- car_manager.py
+++ car_manager.py
@@ -1,15 +1,14 @@
 import random
 from turtle import Turtle
 
-
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
+MOVE_INCREMENT = 3
 
 
 class CarManager(Turtle):
 
-    def __init__(self):
+    def __init__(self, speed_level: int = 1):
         super().__init__()
         self.penup()
         random_index = random.randint(0, 5)
@@ -22,7 +21,14 @@ class CarManager(Turtle):
         random_y = random.randint(-230, 230)
         self.goto(300, random_y)
 
-        self.speed_level = 1
+        self.speed_level = speed_level
 
     def move(self):
-        self.setx(self.xcor() - STARTING_MOVE_DISTANCE - MOVE_INCREMENT * (self.speed_level - 1))
+        if self.xcor() > -300:
+            self.setx(self.xcor() - STARTING_MOVE_DISTANCE - MOVE_INCREMENT * (self.speed_level - 1))
+        else:
+            self.remove_car()
+
+    def remove_car(self):
+        self.goto(-350, 0)
+        self.hideturtle()
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+GAME_TICK = 0.040
+WIN_PAUSE_DURATION = 1.5
+SPAWN_TIME = 0.25
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
@@ -14,22 +18,36 @@ screen.onkeypress(key="Up", fun=player.move)
 
 cars = []
 current_time = time.time()
+level = 1
 
 
-def spawn_car():
+def spawn_car(speed_level):
     global current_time
-    if time.time() - 0.3 > current_time:
+    if time.time() - SPAWN_TIME > current_time:
         current_time = time.time()
-        new_car = CarManager()
+        new_car = CarManager(speed_level)
         cars.append(new_car)
     for car in cars:
         car.move()
 
 
+def check_win():
+    global level
+    if player.ycor() > 280:
+        level += 1
+        for car in cars:
+            car.remove_car()
+        cars.clear()
+        player.goto((0, -280))
+        screen.update()
+        time.sleep(WIN_PAUSE_DURATION)
+
+
 game_is_on = True
 while game_is_on:
-    time.sleep(0.040)
-    spawn_car()
+    time.sleep(GAME_TICK)
+    spawn_car(level)
+    check_win()
     screen.update()
 
 screen.exitonclick()
